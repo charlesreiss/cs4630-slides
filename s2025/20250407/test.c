01d3170d1d2f071f1184b2cd224cf1f0fb8f9f01
Charles Reiss <creiss@virginia.edu>
2025-04-07 12:26:22 -0400

    late edits re: quiz/capabilities

--- s2025/20250407/test.c
+++ s2025/20250407/test.c
@@ -4,5 +4,17 @@
 #include <limits.h>
 #include <string.h>
 #include <errno.h>
+
+#include <curl/curl.h>
 int main() {
+    printf("here\n");
+#if 0
+    FILE *fh = fopen("output.txt", "w");
+    fprintf(fh, "example");
+    fclose(fh);
+#endif
+    CURL *handle = curl_easy_init();
+    if (CURLE_OK != curl_easy_setopt(handle, CURLOPT_URL, "https://www.cs.virginia.edu/~cr4bd/test.txt")) abort();
+    if (CURLE_OK != curl_easy_perform(handle)) abort();
 }
+

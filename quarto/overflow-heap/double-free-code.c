d43c43a44f092786b3da953d685ed6f6ccfc31fa
Charles Reiss <creiss@virginia.edu>
2026-03-13 11:14:47 -0400

    overflow-heap: separate slide code files

---
+++ quarto/overflow-heap/double-free-code.c
@@ -0,0 +1,13 @@
+free(thing);
+free(thing);
+char *p = malloc(...);
+// p points to next/prev
+//   on list of avail.
+//   blocks
+strcpy(p, attacker_controlled);
+malloc(...);
+char *q = malloc(...);
+// q points to attacker-
+//   chosen address
+strcpy(q, attacker_controlled2);
+...
